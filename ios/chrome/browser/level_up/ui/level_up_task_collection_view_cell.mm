9160b638c2860b91f4fa2ecb5d38062213ee92ed
Pier-Alexandre Bouchard <pabouchard@google.com>
2026-06-12 11:22:00 -0700

    [iOS] Add a collapsible header for completed tasks
    Adds `LevelUpCompletedTaskHeaderRowView` to group completed tasks.
    Tapping the header expands or collapses the completed tasks section,
    dynamically resizing the card layout to show or hide the rows.
    
    Screenshot: https://screenshot.googleplex.com/5VsA8wyQJ9FQk9P
    Specs: https://www.figma.com/design/tcUIMogK7sagnPg8smJWX5/Level-Up?node-id=1713-120898&m=dev
    Change-Id: Iafe44e7d3915128237090cf45b389a8fdd25b374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905340
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646115}

--- ios/chrome/browser/level_up/ui/level_up_task_collection_view_cell.mm
+++ ios/chrome/browser/level_up/ui/level_up_task_collection_view_cell.mm
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/level_up/ui/level_up_task_collection_view.h"
+#import "ios/chrome/browser/level_up/ui/level_up_task_collection_view_cell.h"
 
+#import "ios/chrome/browser/level_up/coordinator/level_up_category.h"
 #import "ios/chrome/browser/level_up/coordinator/level_up_task.h"
+#import "ios/chrome/browser/level_up/ui/level_up_completed_task_header_row_view.h"
 #import "ios/chrome/browser/level_up/ui/level_up_task_row_view.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -28,7 +30,9 @@ const CGFloat kCardShadowAlpha = 0.05;
 const CGFloat kLayoutSpacing = 16.0;
 }  // namespace
 
-@implementation LevelUpTaskCollectionView {
+#pragma mark - LevelUpTaskCollectionViewCell
+
+@implementation LevelUpTaskCollectionViewCell {
   UILabel* _titleLabel;
   UIButton* _seeAllButton;
   UIStackView* _rowsStackView;
@@ -59,6 +63,8 @@ const CGFloat kLayoutSpacing = 16.0;
         [UIColor colorNamed:kPrimaryBackgroundColor];
     self.contentView.layer.cornerRadius = kCardCornerRadius;
     self.contentView.layer.masksToBounds = YES;
+    self.contentView.translatesAutoresizingMaskIntoConstraints = NO;
+    AddSameConstraints(self.contentView, self);
 
     self.layer.shadowColor =
         [UIColor colorWithRed:0 green:0 blue:0 alpha:kCardShadowAlpha].CGColor;
@@ -73,17 +79,34 @@ const CGFloat kLayoutSpacing = 16.0;
     _rowsStackView.axis = UILayoutConstraintAxisVertical;
     _rowsStackView.translatesAutoresizingMaskIntoConstraints = NO;
 
+    UIView* stretchSpacer = [[UIView alloc] init];
+    stretchSpacer.translatesAutoresizingMaskIntoConstraints = NO;
+    [stretchSpacer setContentHuggingPriority:1
+                                     forAxis:UILayoutConstraintAxisVertical];
+    [stretchSpacer
+        setContentCompressionResistancePriority:1
+                                        forAxis:UILayoutConstraintAxisVertical];
+
     UIStackView* mainStack = [[UIStackView alloc] initWithArrangedSubviews:@[
-      headerView, separatorView, _rowsStackView
+      headerView, separatorView, _rowsStackView, stretchSpacer
     ]];
     mainStack.axis = UILayoutConstraintAxisVertical;
     mainStack.translatesAutoresizingMaskIntoConstraints = NO;
 
     [self.contentView addSubview:mainStack];
-    AddSameConstraints(mainStack, self.contentView);
+
+    AddSameConstraintsToSides(
+        mainStack, self.contentView,
+        LayoutSides::kTop | LayoutSides::kLeading | LayoutSides::kTrailing);
+    NSLayoutConstraint* stackBottomConstraint = [mainStack.bottomAnchor
+        constraintEqualToAnchor:self.contentView.bottomAnchor];
+    stackBottomConstraint.priority = UILayoutPriorityDefaultHigh - 1;
+    stackBottomConstraint.active = YES;
   }
   return self;
 }
+
+#pragma mark - UICollectionViewCell
 
 - (void)layoutSubviews {
   [super layoutSubviews];
@@ -98,24 +121,58 @@ const CGFloat kLayoutSpacing = 16.0;
   self.delegate = nil;
   _titleLabel.text = nil;
   _seeAllButton.hidden = YES;
-}
-
-- (void)setLevel:(NSInteger)level tasksForLevel:(NSArray<LevelUpTask*>*)tasks {
-  // Clear previous task rows.
   [_rowsStackView.arrangedSubviews
       makeObjectsPerformSelector:@selector(removeFromSuperview)];
+}
 
-  // Populates rows dynamically.
-  for (NSUInteger i = 0; i < tasks.count; i++) {
-    LevelUpTask* task = tasks[i];
-    LevelUpTaskRowView* row = [[LevelUpTaskRowView alloc] init];
-    BOOL showSeparator = i < tasks.count - 1;
-    [row configureWithTask:task showSeparator:showSeparator];
-    [_rowsStackView addArrangedSubview:row];
+- (UICollectionViewLayoutAttributes*)preferredLayoutAttributesFittingAttributes:
+    (UICollectionViewLayoutAttributes*)layoutAttributes {
+  UICollectionViewLayoutAttributes* attributes =
+      [super preferredLayoutAttributesFittingAttributes:layoutAttributes];
+  CGSize fittingSize = [self.contentView
+      systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+  CGRect cellFrame = layoutAttributes.frame;
+  cellFrame.size.height = fittingSize.height;
+  attributes.frame = cellFrame;
+  return attributes;
+}
+
+- (void)setTasks:(NSArray<LevelUpTask*>*)activeTasks
+       completedTasks:(NSArray<LevelUpTask*>*)completedTasks
+    completedExpanded:(BOOL)completedExpanded {
+  [_rowsStackView.arrangedSubviews
+      makeObjectsPerformSelector:@selector(removeFromSuperview)];
+  [self addRows:activeTasks hidden:NO];
+
+  if (completedTasks.count > 0) {
+    [self addHeaderWithCount:completedTasks.count expanded:completedExpanded];
+    [self addRows:completedTasks hidden:!completedExpanded];
   }
 }
 
 #pragma mark - Private
+
+// Create and add task rows to the stack view.
+- (void)addRows:(NSArray<LevelUpTask*>*)tasks hidden:(BOOL)hidden {
+  for (LevelUpTask* task in tasks) {
+    LevelUpTaskRowView* row = [[LevelUpTaskRowView alloc] init];
+    BOOL showSeparator = (task != tasks.lastObject);
+    [row configureWithTask:task showSeparator:showSeparator];
+    row.hidden = hidden;
+    [_rowsStackView addArrangedSubview:row];
+  }
+}
+
+// Add the completed tasks header row to the stack view.
+- (void)addHeaderWithCount:(NSInteger)count expanded:(BOOL)expanded {
+  LevelUpCompletedTaskHeaderRowView* header =
+      [[LevelUpCompletedTaskHeaderRowView alloc] init];
+  [header configureWithCompletedTasksCount:count expanded:expanded];
+  [header addTarget:self
+                action:@selector(didTapCompletedHeader)
+      forControlEvents:UIControlEventTouchUpInside];
+  [_rowsStackView addArrangedSubview:header];
+}
 
 // Creates the tasks card header row.
 - (UIView*)createHeaderView {
@@ -171,9 +228,14 @@ const CGFloat kLayoutSpacing = 16.0;
   return separator;
 }
 
-// Target helper executed when user taps the "See All" action link.
+// Target executed when user taps the "See All" action link.
 - (void)didTapSeeAll {
   [self.delegate didTapSeeAllTasks:self];
+}
+
+// Target executed when user taps the completed header.
+- (void)didTapCompletedHeader {
+  [self.delegate taskCollectionViewDidTapCompletedHeader:self];
 }
 
 @end
