c602757f4e08fff285bba739190e3a52e5acc2f8
Jordan Bayles <jophba@chromium.org>
2026-06-16 11:31:10 -0700

    Media: Default-enable Symphonia FLAC decoding on Desktop
    This default-enables the kSymphoniaAudioDecoding feature on Win, Mac,
    Linux, and ChromeOS since the rollout is complete. It remains in
    experimentation and default disabled on Android and Fuchsia, which are
    expected to launch in M150.
    
    Googlers may view the rollout journal
    (go/symphonia-audio-decoding-journal) for more information about the
    launch decision.
    
    TL;DR Symphonia behaves well and its usage in FLAC decoding does not
    appear to introduce any regressions. Very happy to launch on Desktop.
    
    Bug: 476203635
    Change-Id: I2f022b480d23560d7d3ca560b69e7d80ef4900d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7948180
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647763}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -607,7 +607,15 @@ BASE_FEATURE(kRevokeMediaSourceObjectURLOnAttach,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(ENABLE_SYMPHONIA)
-BASE_FEATURE(kSymphoniaAudioDecoding, base::FEATURE_DISABLED_BY_DEFAULT);
+// Android is expected to launch in M150.
+BASE_FEATURE(kSymphoniaAudioDecoding,
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 BASE_FEATURE(kSymphoniaMp3Decoding, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kSymphoniaPcmDecoding, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kSymphoniaVorbisDecoding, base::FEATURE_DISABLED_BY_DEFAULT);
