bb6818b11e0d6491ba1938b2c416fe422aea698e
Rusty Sammon <sammon@google.com>
2026-06-22 14:13:38 -0700

    Use GetBrowserForMigrationOnly()->override_bounds to check window bounds prior to Window Manager moving it
    BUG=525638651,479420496
    
    Change-Id: I2d2ea6eee150a7fd0177c7204839c8f96a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7974085
    Commit-Queue: Rusty Sammon <sammon@google.com>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650576}

--- chrome/browser/sessions/encrypted_session_storage_browsertest.cc
+++ chrome/browser/sessions/encrypted_session_storage_browsertest.cc
@@ -74,7 +74,6 @@
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/base/ozone_buildflags.h"
 
 namespace sessions {
 
@@ -680,8 +679,11 @@ class SessionRestoreAcrossStagesTest
     auto [expected_bounds1, expected_bounds2] =
         ReadWindowBoundsFromFile(bounds_file);
 
-    // Assert Window 1 bounds
-    EXPECT_EQ(expected_bounds1, w1->GetWindow()->GetBounds());
+    // Assert Window 1 bounds.
+    // Downcast via GetBrowserForMigrationOnly() is required to inspect
+    // requested bounds prior to Window Manager modification.
+    EXPECT_EQ(expected_bounds1,
+              w1->GetBrowserForMigrationOnly()->override_bounds());
 
     // Window 1 Tab 1 shows GetUrl(1) and is active
     TabStripModel* w1_model = w1->GetTabStripModel();
@@ -696,8 +698,9 @@ class SessionRestoreAcrossStagesTest
     EXPECT_EQ(GetUrl(2), controller.GetEntryAtIndex(0)->GetURL());
     EXPECT_EQ(GetUrl(3), controller.GetEntryAtIndex(1)->GetURL());
 
-    // Assert Window 2 bounds
-    EXPECT_EQ(expected_bounds2, w2->GetWindow()->GetBounds());
+    // Assert Window 2 bounds.
+    EXPECT_EQ(expected_bounds2,
+              w2->GetBrowserForMigrationOnly()->override_bounds());
 
     TabStripModel* w2_model = w2->GetTabStripModel();
     // Tab 1 should be pinned and shows GetUrl(1)
@@ -752,13 +755,7 @@ IN_PROC_BROWSER_TEST_F(SessionRestoreAcrossStagesTest, PRE_Restore) {
   AssertCommandStorageBackendFilesExist(SessionType::kSessionRestore);
 }
 
-// TODO(crbug.com/525638651): Re-enable this test on Linux Wayland.
-#if BUILDFLAG(IS_LINUX) && BUILDFLAG(SUPPORTS_OZONE_WAYLAND)
-#define MAYBE_Restore DISABLED_Restore
-#else
-#define MAYBE_Restore Restore
-#endif
-IN_PROC_BROWSER_TEST_F(SessionRestoreAcrossStagesTest, MAYBE_Restore) {
+IN_PROC_BROWSER_TEST_F(SessionRestoreAcrossStagesTest, Restore) {
   AssertSessionState();
 }
 
