2f04bdc6d755da4c974372fead43034f468b87ba
Timothy Loh <timloh@chromium.org>
2026-06-24 00:22:25 -0700

    Revert "Roll src/third_party/snappy/src/ 32ded457c..27ab5f7f5 (33 commits)"
    This reverts commit ba185b5e2ddc3763ea1911e27d332a2c38195875.
    
    Reason for revert: Compile failing on chromeos-volteer-chrome
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-volteer-chrome/53876/overview
    
    Original change's description:
    > Roll src/third_party/snappy/src/ 32ded457c..27ab5f7f5 (33 commits)
    >
    > https://chromium.googlesource.com/external/github.com/google/snappy.git/+log/32ded457c0b1..27ab5f7f5184
    >
    > [AI Generated] Following fix is generated via AI agent. Please review it carefully
    >
    > $ git log 32ded457c..27ab5f7f5 --date=short --no-merges --format='%ad %ae %s'
    > 2026-05-09 danilak Fix benchmark C++11 code
    > 2026-05-09 danilak Fix minor changes
    > 2026-05-09 danilak Import changes
    > 2026-03-18 no-reply Replace usage of ARCH_ARM with standard compiler macros.
    > 2026-02-24 yichengluo Export LICENSE in bazel
    > 2025-10-21 danilak Internal changes
    > 2025-03-27 danilak zippy: Fix data corruption when compressed output is more than 4GB
    > 2024-05-07 danilak Internal changes
    > 2024-04-22 danilak Internal change
    > 2024-04-19 connaldesouza Optimize ExtractOffset() by avoiding storing the mask.
    > 2024-04-09 danilak Internal change
    > 2024-04-08 danilak Internal change
    > 2024-04-04 rjogrady Internal changes
    > 2026-04-17 zheng.ziyang RISC-V: Optimize Snappy decompression tag advance (AdvanceToNextTagRVOptimized) for +4% throughput
    > 2026-04-15 zheng.ziyang refactor: Align RISC-V implementation with AArch64 branchless mask extraction
    > 2026-03-06 github Move `//:config` to `implementation_deps` to prevent header collision
    > 2026-02-28 github Add Bazel 9 support
    > 2026-01-21 166056179+danilak-G Create COPYING.notestdata
    > 2025-12-12 xdzyf2012 limit RISC-V FindMatchLength optimizations to 64-bit
    > 2025-11-10 zheng.ziyang add zbb check macro
    > 2025-11-10 zheng.ziyang add zbb check macro
    > 2025-10-20 danilak Fix an unused var warning for RISC-V
    > 2025-10-17 zheng.ziyang fix by some comments
    > 2025-08-28 zheng.ziyang add RVV support and optmized uncompress speed
    > 2025-08-28 zheng.ziyang add RVV support and optimized uncompress speed
    > 2025-07-29 zheng.ziyang Use high-performance FindMatchLength to Optimize Snappy compression speed For RISC-V[skip ci]
    > 2025-07-29 zheng.ziyang Use high-performance FindMatchLength to Optimize Snappy compression speed For RISC-V[skip ci]
    > 2025-07-01 wang.han15 build: Update benchmark submodule for RISC-V - Pins to b20cea67 to fix illegal instruction - Verified on openEuler RISC-V
    > 2025-03-26 danilak Release 1.2.2
    > 2025-03-26 dhaney Export symbols from snappy_test_support when building shared
    > 2025-03-26 danilak Fix data corruption when output of snappy compression is more than 4GB
    > 2025-03-21 foss [cmake] Require CMake version 3.10 or newer
    > 2025-01-31 rp42 Support clang-cl on Windows
    >
    > Bug: 493761705
    >
    > Created with:
    >   roll-dep src/third_party/snappy/src
    >
    > Change-Id: If05e97c9ca9d8ff08eb05770c9e507c51279a922
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7916978
    > Reviewed-by: Jiewei Qian <qjw@chromium.org>
    > Commit-Queue: Archit Jain <architjain@google.com>
    > Reviewed-by: Fergal Daly <fergal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1651521}
    
    Bug: 493761705
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5dfa03bd2bc84294fd90eaf4b0f721f13c80a276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7990275
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Timothy Loh <timloh@chromium.org>
    Owners-Override: Timothy Loh <timloh@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1651540}

--- third_party/snappy/mac/config.h
+++ third_party/snappy/mac/config.h
@@ -1,8 +1,6 @@
 #ifndef THIRD_PARTY_SNAPPY_OPENSOURCE_CMAKE_CONFIG_H_
 #define THIRD_PARTY_SNAPPY_OPENSOURCE_CMAKE_CONFIG_H_
 
-#include <functional>
-
 /* Define to 1 if the compiler supports __attribute__((always_inline)). */
 #define HAVE_ATTRIBUTE_ALWAYS_INLINE 1
 
@@ -58,19 +56,4 @@
    first (like Motorola and SPARC, unlike Intel and VAX). */
 /* #undef SNAPPY_IS_BIG_ENDIAN */
 
-/* Define to 1 if the compiler supports __builtin_prefetch. */
-#define HAVE_BUILTIN_PREFETCH 1
-
-/* Define to 1 if you target processors with SSE4.2 and have <crc32intrin.h>. */
-#define SNAPPY_HAVE_X86_CRC32 0
-
-/* Define to 1 if you target processors with RVV1.0 and have <riscv_vector.h>. */
-#define SNAPPY_RVV_1 0
-
-/* Define to 1 if you target processors with RVV0.7 and have <riscv_vector.h>. */
-#define SNAPPY_RVV_0_7 0
-
-/* Define to 1 if you have <arm_neon.h> and <arm_acle.h> and want to optimize
-   compression speed by using __crc32cw from <arm_acle.h>. */
-#define SNAPPY_HAVE_NEON_CRC32 0
 #endif  // THIRD_PARTY_SNAPPY_OPENSOURCE_CMAKE_CONFIG_H_
