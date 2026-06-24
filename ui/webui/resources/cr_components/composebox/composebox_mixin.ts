427e82dcc6d71419652bbaae0c25a158bee46c94
Nihar Majmudar <niharm@google.com>
2026-06-23 17:57:21 -0700

    Reland "[composebox] Don't shrink composebox once expanded."
    This is a reland of commit 5e566eb628b7778ee61d7dca0e6f86025d1d4edd
    
    Removed failing test since CL adds appropriate tests and the removed test doesn't fully make sense with the new behavior.
    
    Original change's description:
    > [composebox] Don't shrink composebox once expanded.
    >
    > This change makes it so once the composebox expands to a certain height,
    > it doesn't shrink if text is deleted/smart compose text is hidden. The
    > only way to reset the height if if the "x" button is clicked.
    >
    > Also adds padding-block to input, mirror, and smart compose divs to
    > remove flickering when accepting smart compose text in realbox/omnibox.
    >
    > NO_IFTTT=Added in both composebox.ts and composebox_mixin.ts
    >
    > After: https://screencast.googleplex.com/cast/NTc0MTU4NzY2ODY2NDMyMHxiM2VmM2I0Mi02Zg
    > Bug: 524694141
    > Change-Id: Icfe397dd18951765c6d8ba628e2b12ecfaf1a68c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951584
    > Commit-Queue: Nihar Majmudar <niharm@google.com>
    > Reviewed-by: Marlon Facey <mfacey@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1647968}
    
    Bug: 524694141
    Change-Id: I139a46ebe61bbf94a5b615fa541f705828da0f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7973720
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651416}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -1524,6 +1524,7 @@ export const ComposeboxEmbedderMixin =
         clearAllInputs(
             querySubmitted: boolean, shouldBlockAutoSuggestedTabs: boolean) {
           this.clearInput();
+          this.getInputElement().resetHeight();
           // Let `querySubmit` handle clearing files if the tool mode is a tool
           // mode that should be cleared after submitting. For all other general
           // clearing, clear input here.
