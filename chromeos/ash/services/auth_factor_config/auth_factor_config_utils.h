be8e460510f497334cf34a19880a8f6fa16669cf
Ivan Šandrk <isandrk@chromium.org>
2026-06-25 09:37:13 -0700

    Move ObtainContextOrFail helper to auth_factor_config_utils
    This is the first step in unifying context obtaining across auth factor
    editors.
    
    Change-Id: I3765c7660d39ce496f242bf4859abb3cde561234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7942234
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652489}

--- chromeos/ash/services/auth_factor_config/auth_factor_config_utils.h
+++ chromeos/ash/services/auth_factor_config/auth_factor_config_utils.h
@@ -5,6 +5,16 @@
 #ifndef CHROMEOS_ASH_SERVICES_AUTH_FACTOR_CONFIG_AUTH_FACTOR_CONFIG_UTILS_H_
 #define CHROMEOS_ASH_SERVICES_AUTH_FACTOR_CONFIG_AUTH_FACTOR_CONFIG_UTILS_H_
 
+#include <memory>
+#include <string>
+
+#include "base/functional/callback.h"
+#include "base/location.h"
+#include "base/types/expected.h"
+#include "chromeos/ash/components/login/auth/public/user_context.h"
+#include "chromeos/ash/components/osauth/public/auth_session_storage.h"
+#include "chromeos/ash/services/auth_factor_config/public/mojom/auth_factor_config.mojom.h"
+
 namespace cryptohome {
 class AuthFactor;
 }
@@ -14,6 +24,50 @@ namespace ash::auth {
 bool IsGaiaPassword(const cryptohome::AuthFactor& factor);
 
 bool IsLocalPassword(const cryptohome::AuthFactor& factor);
+
+void FailWithInvalidTokenError(
+    base::Location from_here,
+    base::OnceCallback<void(mojom::ConfigureResult)> result_callback);
+
+void FailWithInvalidTokenError(
+    base::Location from_here,
+    base::OnceCallback<
+        void(base::expected<mojom::PasswordComplexity, mojom::ConfigureResult>)>
+        result_callback);
+
+template <typename ResultCallback, typename Continuation>
+void OnContextBorrowed(base::Location from_here,
+                       ResultCallback result_callback,
+                       Continuation continuation_callback,
+                       std::unique_ptr<UserContext> context) {
+  if (!context) {
+    FailWithInvalidTokenError(from_here, std::move(result_callback));
+    return;
+  }
+  std::move(continuation_callback)
+      .Run(std::move(result_callback), std::move(context));
+}
+
+template <typename ResultCallback, typename Continuation>
+void ObtainContextOrFailImpl(base::Location from_here,
+                             const std::string& auth_token,
+                             ResultCallback result_callback,
+                             Continuation continuation_callback) {
+  if (!ash::AuthSessionStorage::Get()->IsValid(auth_token)) {
+    FailWithInvalidTokenError(from_here, std::move(result_callback));
+    return;
+  }
+  ash::AuthSessionStorage::Get()->BorrowAsync(
+      from_here, auth_token,
+      base::BindOnce(&OnContextBorrowed<ResultCallback, Continuation>,
+                     from_here, std::move(result_callback),
+                     std::move(continuation_callback)));
+}
+
+#define ObtainContextOrFail(auth_token, result_callback,                     \
+                            continuation_callback)                           \
+  ash::auth::ObtainContextOrFailImpl(FROM_HERE, auth_token, result_callback, \
+                                     continuation_callback)
 
 }  // namespace ash::auth
 
