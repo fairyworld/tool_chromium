1bfd9924684243915216869be41013fa99bbc2f3
Olivier Robin <olivierrobin@chromium.org>
2026-06-25 02:30:00 -0700

    Revert "[STTS] Support device-specific mode in SendTabToSelfActivity"
    This reverts commit 9e380b840b694a5a4a88144c2116b06ed5746c2d.
    
    Reason for revert: Break iOS compilation
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-device/494961/overview
    
    Original change's description:
    > [STTS] Support device-specific mode in SendTabToSelfActivity
    >
    > This CL introduces a new device-specific initialization mode to
    > SendTabToSelfActivity, allowing it to represent a single, specific
    > target device in the Share Sheet.
    >
    > The activity's execution triggers the direct-send coordinator command.
    >
    > Envisioned UX flow: http://screencast/cast/NjA0MzI5ODk1NDIxNTQyNHw5YzUwNTMzYS03MA
    >
    > Bug: 493866369
    > Change-Id: I2c4db7eae1b42352d7e772f16301a5c99eea3dee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7978561
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Michael Tatarski <mtatarski@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1652251}
    
    Bug: 493866369
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I10f59bd9a45ac5cbc50e695e0a0255d350693900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8003098
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Reviewed-by: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652255}

--- ios/chrome/browser/sharing/ui_bundled/activity_services/activities/send_tab_to_self_activity.h
+++ ios/chrome/browser/sharing/ui_bundled/activity_services/activities/send_tab_to_self_activity.h
@@ -5,47 +5,20 @@
 #ifndef IOS_CHROME_BROWSER_SHARING_UI_BUNDLED_ACTIVITY_SERVICES_ACTIVITIES_SEND_TAB_TO_SELF_ACTIVITY_H_
 #define IOS_CHROME_BROWSER_SHARING_UI_BUNDLED_ACTIVITY_SERVICES_ACTIVITIES_SEND_TAB_TO_SELF_ACTIVITY_H_
 
-#import "components/sync_device_info/device_info.h"
 #import "ios/chrome/browser/sharing/ui_bundled/activity_services/activities/chrome_activity.h"
 
 @protocol BrowserCoordinatorCommands;
 @class ShareToData;
 
-// Activity representing the generic "Send to your devices" action, which
-// opens a target device selector picker UI when executed. Displayed in the
-// Action (bottom) row of the Share Sheet.
+// Activity that sends the tab to another of the user's devices.
 @interface SendTabToSelfActivity : ChromeActivity
 
 // Initializes the send tab to self activity with the given `data` and the
-// `handler` that is used to add the tab to the other device. This initializer
-// is used for the generic picker entry point.
+// `handler` that is used to add the tab to the other device.
 - (instancetype)initWithData:(ShareToData*)data
                      handler:(id<BrowserCoordinatorCommands>)handler
     NS_DESIGNATED_INITIALIZER;
-
 - (instancetype)init NS_UNAVAILABLE;
-
-@end
-
-// Subclass representing a direct-send shortcut to a specific target device,
-// executing a background send transaction immediately when executed. Displayed
-// in the Share (top) row of the Share Sheet.
-@interface SendTabToSelfShareActivity : SendTabToSelfActivity
-
-// Initializes the send tab to self activity in device-specific mode,
-// sending the tab directly to that target device with the given
-// `activityTitle`, `deviceName`, `cacheGUID`, and `formFactor`.
-- (instancetype)initWithData:(ShareToData*)data
-                     handler:(id<BrowserCoordinatorCommands>)handler
-               activityTitle:(NSString*)activityTitle
-                  deviceName:(NSString*)deviceName
-                   cacheGUID:(NSString*)cacheGUID
-                  formFactor:(syncer::DeviceInfo::FormFactor)formFactor
-    NS_DESIGNATED_INITIALIZER;
-
-- (instancetype)initWithData:(ShareToData*)data
-                     handler:(id<BrowserCoordinatorCommands>)handler
-    NS_UNAVAILABLE;
 
 @end
 
