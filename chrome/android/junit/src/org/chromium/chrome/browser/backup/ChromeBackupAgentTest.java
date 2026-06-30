01cc8b7804b666557dd8e96b23cf7f08d5647e00
Chirag Arora <heychirag@google.com>
2026-06-29 18:13:19 -0700

    Revert "[Android] add setMetricsReportingLevel API in native Java code."
    Reverted CL: crrev.com/c/7852457
    
    This change is being reverted because we have changed the backend
    implementation approach.
    
    Bug: b:483043192
    Change-Id: I06fdbb1c255f72de547c3fbfec4f22d35d3ba2dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7992876
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654520}

--- chrome/android/junit/src/org/chromium/chrome/browser/backup/ChromeBackupAgentTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/backup/ChromeBackupAgentTest.java
@@ -1115,7 +1115,7 @@ public class ChromeBackupAgentTest {
             boolean permittedByUser,
             boolean permittedByPolicy,
             int level,
-            boolean levelDisabledByPolicy)
+            boolean levelEnforcedByPolicy)
             throws IOException {
         BackupDataInput backupData = mock(BackupDataInput.class);
         ArrayList<Pair<String, byte[]>> keysAndValues = new ArrayList<>();
@@ -1139,7 +1139,7 @@ public class ChromeBackupAgentTest {
         keysAndValues.add(
                 booleanPrefPair(
                         ChromePreferenceKeys.PRIVACY_METRICS_REPORTING_DISABLED_BY_POLICY,
-                        levelDisabledByPolicy));
+                        levelEnforcedByPolicy));
 
         when(backupData.getKey())
                 .thenAnswer(
@@ -1201,7 +1201,7 @@ public class ChromeBackupAgentTest {
                         /* permittedByUser= */ false,
                         /* permittedByPolicy= */ false,
                         /* level= */ MetricsReportingLevel.BASIC,
-                        /* levelDisabledByPolicy= */ false);
+                        /* levelEnforcedByPolicy= */ false);
 
         try (ParcelFileDescriptor newState =
                 ParcelFileDescriptor.open(
@@ -1225,7 +1225,7 @@ public class ChromeBackupAgentTest {
                         /* permittedByUser= */ false,
                         /* permittedByPolicy= */ false,
                         /* level= */ MetricsReportingLevel.ADVANCED,
-                        /* levelDisabledByPolicy= */ false);
+                        /* levelEnforcedByPolicy= */ false);
 
         try (ParcelFileDescriptor newState =
                 ParcelFileDescriptor.open(
@@ -1249,7 +1249,7 @@ public class ChromeBackupAgentTest {
                         /* permittedByUser= */ false,
                         /* permittedByPolicy= */ false,
                         /* level= */ MetricsReportingLevel.NONE,
-                        /* levelDisabledByPolicy= */ true);
+                        /* levelEnforcedByPolicy= */ true);
 
         try (ParcelFileDescriptor newState =
                 ParcelFileDescriptor.open(
@@ -1271,7 +1271,7 @@ public class ChromeBackupAgentTest {
                         /* permittedByUser= */ true,
                         /* permittedByPolicy= */ true,
                         /* level= */ MetricsReportingLevel.NONE,
-                        /* levelDisabledByPolicy= */ false);
+                        /* levelEnforcedByPolicy= */ false);
 
         try (ParcelFileDescriptor newState =
                 ParcelFileDescriptor.open(
